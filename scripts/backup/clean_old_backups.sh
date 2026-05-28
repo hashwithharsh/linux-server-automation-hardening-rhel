2d8cab62282632402f2148d8ced43f25a2eb90a6
Harsh Yadav <harshyadav.devops@gmail.com>
2026-05-28 11:47:29 +0530

    make backup cleanup accept retention days and print what is removed

--- scripts/backup/clean_old_backups.sh
+++ scripts/backup/clean_old_backups.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-find /var/backups -name "*.tar.gz" -mtime +7 -delete
+BACKUP_DIR="${BACKUP_DIR:-/var/backups}"
+DAYS="${1:-7}"
+echo "Removing backups older than $DAYS days from $BACKUP_DIR"
+find "$BACKUP_DIR" -name "*.tar.gz" -mtime +"$DAYS" -print -delete
