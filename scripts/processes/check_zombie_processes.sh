1c69f0cc31aea08f9ba14f7018c4667a8ba50f1f
Harsh Yadav <harshyadav.devops@gmail.com>
2026-06-02 22:12:47 +0530

    improve zombie check output and handle empty case

--- scripts/processes/check_zombie_processes.sh
+++ scripts/processes/check_zombie_processes.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-ps aux | awk '$8=="Z"'
+ZOMBIES=$(ps -eo pid,ppid,state,cmd | awk '$3=="Z"')
+if [ -z "$ZOMBIES" ]; then
+  echo "No zombie processes found"
+else
+  echo "=== Zombie processes ==="
+  echo "$ZOMBIES"
+fi
