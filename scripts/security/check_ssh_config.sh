30334114dc4e61fd8b25758808f09aa4c78ee34e
Harsh Yadav <harshyadav.devops@gmail.com>
2026-06-03 15:22:38 +0530

    use sshd -T for effective config values instead of raw file

--- scripts/security/check_ssh_config.sh
+++ scripts/security/check_ssh_config.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-grep -E '^(PermitRootLogin|PasswordAuthentication|Port)' /etc/ssh/sshd_config
+CONF=/etc/ssh/sshd_config
+echo "=== Important SSH settings ==="
+for key in PermitRootLogin PasswordAuthentication PubkeyAuthentication Port ChallengeResponseAuthentication; do
+  val=$(sshd -T 2>/dev/null | awk -v k=$(echo $key | tr 'A-Z' 'a-z') '$1==k {print $2}')
+  printf "%-30s %s\n" "$key" "${val:-not set}"
+done
